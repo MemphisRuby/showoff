b774748a23c0734d4060fc14f3373ef0fca38581
Daniel J. Pritchett <dpritchett@gmail.com>
2015-02-06 19:47:08 -0600

    I have no idea what I'm doing with AMS

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,11 +1,9 @@
 class HelloWorldController < ApplicationController
   def index
-    @groups = Group.all.limit(10)
-    @events = Event.all.limit(10)
+    @groups   = Group.all.limit(10)
+    @events   = Event.all.limit(10)
+    @projects = Project.all.limit(10)
 
-    render json: {
-      groups: @groups,
-      events: @events
-    }
+    render json: @projects, include: 'groups,events'
   end
 end
